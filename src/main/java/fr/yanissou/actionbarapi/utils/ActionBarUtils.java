ed23508bc3ca95da85831eb7204bbbb908c4f83a
Yanissou <yanissoumc@gmail.com>
2025-01-12 23:15:07 +0100

    Switched to NMS

--- src/main/java/fr/yanissou/actionbarapi/utils/ActionBarUtils.java
+++ src/main/java/fr/yanissou/actionbarapi/utils/ActionBarUtils.java
@@ -1,9 +1,9 @@
 package fr.yanissou.actionbarapi.utils;
 
-import net.minecraft.server.v1_8_R3.IChatBaseComponent;
-import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class ActionBarUtils {
 
@@ -13,8 +13,32 @@ public class ActionBarUtils {
      * @param message the message to send
      */
     public static void sendActionBar(Player player, String message) {
-        String jsonMessage = "{\"text\":\"" + message + "\"}";
-        PacketPlayOutChat packet = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a(jsonMessage), (byte) 2);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+        try {
+            // Obtenir CraftPlayer et son handle
+            final Object craftPlayer = player.getClass().getMethod("getHandle").invoke(player);
+            final Object playerConnection = craftPlayer.getClass().getField("playerConnection").get(craftPlayer);
+
+            // Obtenir IChatBaseComponent
+            final Class<?> chatBaseComponentClass = getNMSClass("IChatBaseComponent");
+            final Class<?> chatSerializerClass = chatBaseComponentClass.getDeclaredClasses()[0]; // Classe interne ChatSerializer
+            final Method chatSerializerMethod = chatSerializerClass.getMethod("a", String.class);
+            final Object chatBaseComponent = chatSerializerMethod.invoke(null, "{\"text\":\"" + message + "\"}");
+
+            // Construire le packet PacketPlayOutChat
+            final Class<?> packetPlayOutChatClass = getNMSClass("PacketPlayOutChat");
+            final Constructor<?> packetConstructor = packetPlayOutChatClass.getConstructor(chatBaseComponentClass, byte.class);
+            final Object packet = packetConstructor.newInstance(chatBaseComponent, (byte) 2);
+
+            // Envoyer le packet
+            final Method sendPacketMethod = playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet"));
+            sendPacketMethod.invoke(playerConnection, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Class<?> getNMSClass(String className) throws ClassNotFoundException {
+        final String version = org.bukkit.Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+        return Class.forName("net.minecraft.server." + version + "." + className);
     }
 }
